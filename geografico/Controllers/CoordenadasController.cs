6d1cf295239b5bbb9195c8287cb00f4a8b5f7a4d
Luis Alberto Fernández Abeleira <portico1970@yahoo.es>
2023-05-21 12:37:22 +0200

    Agregar archivos de proyecto.

---
+++ geografico/Controllers/CoordenadasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace geografico.Controllers
+{
+    public class CoordenadasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
